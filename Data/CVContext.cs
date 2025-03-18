614f88cffbaef492774d0c245fc53855f702510a
ikariLain <matheus-torrico@outlook.com>
2025-03-18 14:30:25 +0100

    Add CVContext and remove unecessary content in program.cs

---
+++ Data/CVContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using REST_API_för_CV_hantering.Models;
+
+namespace REST_API_för_CV_hantering.Data
+{
+    public class CVContext : DbContext
+    {
+        public CVContext(DbContextOptions<CVContext> options) : base(options)
+        {
+
+        }
+
+        // This is the table that will be created in the database
+
+        DbSet<Person> Persons { get; set; }
+
+        DbSet<Experience> Experience { get; set; }
+
+        public DbSet<Education> Educations { get; set; }
+
+    }
+}
