15171c54d75363107cf90733934900f708325b51
ikariLain <matheus-torrico@outlook.com>
2025-03-21 15:00:18 +0100

    Experience DTO

---
+++ DTOs/ExperienceDTOs/ExperienceDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REST_API_för_CV_hantering.DTOs.ExperienceDTO
+{
+    public class PersonCreateDTO
+    {
+        public string Company { get; set; }
+        public string JobTitle { get; set; }
+        public string Decription { get; set; }
+        public int StartDate { get; set; }
+        public int? EndDate { get; set; }
+    }
+}
